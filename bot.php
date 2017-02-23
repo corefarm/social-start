d1febb36b8a900c6b5010f11dd0c9150945b06e3
theskyxx <thesky.x@gmail.com>
2017-02-23 18:04:54 +0700

    เทสส

--- bot.php
+++ bot.php
@@ -1,4 +1,3 @@
-<?
 <?php
 $access_token = 'ki/sALGeAGtfPJsCbQY+Ama0bBSByknlDdsU32D1fnAGwt2/L9KqasU/HxA9ojgPHNcSaItAV2cJEasYBZj1qQ+dZOEt7ZKaTz/OG7ZZNISFHh4NWE/P5Mg7hX84D+AZtaYHVjv2VS9oQiObD6Kl+QdB04t89/1O/w1cDnyilFU=';
 
