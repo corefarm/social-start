b9436d56b90a92e9a602a37548daccf6f07a2af7
theskyxx <thesky.x@gmail.com>
2017-02-23 21:38:23 +0700

    54

--- bot.php
+++ bot.php
@@ -20,7 +20,7 @@ if (!is_null($events['events'])) {
 		
 			$messages = [
 					'type' => 'text',
-					'text' => 'ตอบจาก  Bot v2 : '.$text 
+					'text' => 'ตอบจาก  Bot v2 : '.$text.' to key'.$replyToken 
 			];	
 					
 
@@ -104,27 +104,7 @@ if (!is_null($events['events'])) {
 
 			echo $result . '\r\n';
 			
-			$messages = [
-						'type' => 'text',
-						'text' => 'ตอบจาก  Bot v2 result : '.$result
-				];		
-
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);	 			
+			
 		}
 	}
 }
