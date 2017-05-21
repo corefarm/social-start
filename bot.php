584a3dab94fd265950ebb75abca11980d3acc4b0
theskyxx <thesky.x@gmail.com>
2017-05-21 23:12:08 +0700

    rtrt

--- bot.php
+++ bot.php
@@ -29,7 +29,7 @@ $content = file_get_contents('php://input');
 
 $content_sql = $content;
 
-$content_sql   = str_replace('"', "\"", $content_sql);
+$content_sql   = str_replace('"', '\"', $content_sql);
 
 $sql = " INSERT INTO \"Fr_User_Log\"(
 	\"Request\", \"ReplyConfirm\", \"CreateDate\")
