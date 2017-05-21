0a6d4a52d079d7a28d5258540017549185e473a8
theskyxx <thesky.x@gmail.com>
2017-05-21 22:22:56 +0700

    d

--- testDb.php
+++ testDb.php
@@ -10,13 +10,17 @@ function pg_connection_string_from_database_url() {
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+//$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+
+print "bf insert \n "."INSERT INTO public.'Fr_User_Log'(
+	'Request', 'ReplyConfirm', 'CreateDate')
+	VALUES ('test', 'test', now())";	
 
 $result = pg_query($pg_conn, "INSERT INTO public.'Fr_User_Log'(
 	'Request', 'ReplyConfirm', 'CreateDate')
 	VALUES ('test', 'test', now())");
 	
-print "<pre>\n".$result;	
+print "af insert \n ".$result;	
 /* print "<pre>\n";
 if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
