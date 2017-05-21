68584945a0019ff67dc6b9bd824198f22d392bb3
theskyxx <thesky.x@gmail.com>
2017-05-21 23:53:13 +0700

    wwww

--- bot.php
+++ bot.php
@@ -1,21 +1,19 @@
 <?php
 
 
-function writeData($sql) {
-    # This function reads your DATABASE_URL config var and returns a connection
-# string suitable for pg_connect. Put this in your app.
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["DATABASE_URL"]));
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
+
+function writeData($sql) {
+    # This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
+
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 	$result = pg_query($pg_conn, $sql);
-
-
-
-
 Return $result;
 
 }
