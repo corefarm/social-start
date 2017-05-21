138fffaea3a10d3958cb0492a41b2b9f03415d9a
theskyxx <thesky.x@gmail.com>
2017-05-21 22:43:52 +0700

    2

--- testDb.php
+++ testDb.php
@@ -10,17 +10,17 @@ function pg_connection_string_from_database_url() {
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+//$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
 
-echo $result."<BR/> ";
+//echo $result."<BR/> ";
 
-echo "bf insert \n INSERT INTO public.''Fr_User_Log''(
+$sql = " INSERT INTO ''Fr_User_Log''(
 	''Request'', ''ReplyConfirm'', ''CreateDate'')
-	VALUES ('test', 'test', now()) <BR/> ";	
+	VALUES ('test', 'test', now()) ";
 
-$result = pg_query($pg_conn, "INSERT INTO public.''Fr_User_Log''(
-	''Request'', ''ReplyConfirm'', ''CreateDate'')
-	VALUES ('test', 'test', now()) ");
+echo "bf insert \n ".$sql." <BR/> ";	
+
+$result = pg_query($pg_conn, $sql);
 	
 echo "af insert  ".$result;	
 /* print "<pre>\n";
