e94cd4c01d3d48923292acb096f45421cbbc0a83
b6db07d1a5 <sitthichai.wa@gmail.com>
2017-06-05 16:29:19 +0700

    change to code

--- bot.php
+++ bot.php
@@ -1038,7 +1038,7 @@ function retrieveMsgProduct($obj) {
 		foreach ($arrData as $val) {
 			array_push($arrMessageDs,[
 				'type' => 'postback',
-				'label' => $val['Product_Name'],
+				'label' => $val['Product_Code'],
 				'data' => 'action=buy&itemid=123',
 				'text' => '<เบอร์อาหาร> '.$val['Product_Code'],
 			]);
