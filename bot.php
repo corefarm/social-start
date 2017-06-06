618fa3928a97c886da9445dd00e844622c10c23f
b6db07d1a5 <sitthichai.wa@gmail.com>
2017-06-06 13:57:45 +0700

    long name

--- bot.php
+++ bot.php
@@ -1036,7 +1036,7 @@ function retrieveMsgProduct($obj) {
 		foreach ($arrData as $val) {
 			array_push($arrMessageDs,[
 				'type' => 'postback',
-				'label' => $val['Product_Name'],
+				'label' => str_replace(' ','',$val['Product_Name']),
 				'data' => 'action=buy&itemid=123',
 				'text' => '<เบอร์อาหาร>'.$val['Product_Code'],
 			]);
