1f81430ee92304fb1447ff548cb0b5e8adf0293a
b6db07d1a5 <sitthichai.wa@gmail.com>
2017-06-12 10:04:01 +0700

    re

--- report/report.php
+++ report/report.php
@@ -8,12 +8,14 @@ echo "Hello World!";
 
 $url = 'https://mservice-uat.cpf.co.th/Farm/FarmMobileRestService/FarmMobileRestService.svc/json/getreportswstock/123456789,2000020032-0-1-4-36,20170601,20170630'
 
-//$arrContextOptions = array( 'ssl' => array( 'verify_peer' => false,'verify_peer_name' => false )); 
+$arrContextOptions = array( 
+					'ssl' => array( 
+						'verify_peer' => false,
+						'verify_peer_name' => false )); 
 
-$content = file_get_contents($url,false, stream_context_create(array( 'ssl' => array( 'verify_peer' => false,'verify_peer_name' => false ))));
+$content = file_get_contents($url,false, stream_context_create($arrContextOptions));
 $result = json_decode($content, true);
 
-echo json_encode($result[$obj['GetReportSWStockResult']]);
 
 ?>
 
