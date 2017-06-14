17106e53c4e21fd394ccda98e4db269f73952031
b6db07d1a5 <sitthichai.wa@gmail.com>
2017-06-14 14:01:17 +0700

    re

--- bot.php
+++ bot.php
@@ -788,24 +788,25 @@ if (!is_null($events['events'])) {
 			}
 			
 			if(stristr($text,'<รายงาน>') ) {
-				$ret = [
-
-					'type' => 'template',
-					'altText' => 'this is a buttons template',
-					'template' => [
-						'type' => 'buttons',
-						'title' => 'กรุณาเลือกเล้า',
-						'text' => 'Please select pen.',
-						'actions' => [
-							'type' => 'uri',
-							'label' => 'รายงาน',
-							'uri' => 'https://shielded-dawn-30361.herokuapp.com/report/report.php', 
-						]
-					]
-
-				];	
 				
-				array_push($msg,$ret);
+				$msgReport = [
+						'type' => 'template',
+						'altText' => 'this is a buttons template',
+						'template' => [
+							'type' => 'buttons',
+							'title' => 'report',
+							'text' => 'Please select date.',
+							'actions' => array(
+								[
+									'type' => 'uri',
+									'label' => 'รายงาน',
+									'uri' => 'https://shielded-dawn-30361.herokuapp.com/report/report.php', 
+								]
+							)
+						]
+				];
+				
+				array_push($msg,$msgReport);
 			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
