f5e94ea329a0e0c2beec916ad12f2549104d493b
b6db07d1a5 <sitthichai.wa@gmail.com>
2017-06-14 11:24:05 +0700

    re

--- report/report.php
+++ report/report.php
@@ -3,16 +3,19 @@
 
 <h1>My first PHP page</h1>
 
-
+    <label id="lblHeader"></label>
+    <div id="divFilter">
+        </div>
+    <div class="row" id="containTbData">
+        </div>
 
 </body>
 
 <script>
 
 	<?php
-		echo "Hello World!";
 
-		$url = 'https://mservice-uat.cpf.co.th/Farm/FarmMobileRestService/FarmMobileRestService.svc/json/getreportswstock/123456789,2000020032-0-1-4-36,20170601,20170630';
+        $url = 'https://mservice-uat.cpf.co.th/Farm/FarmMobileRestService/FarmMobileRestService.svc/json/getreportswstock/123456789,2000020032-0-1-4-36,20170601';
 
 		$arrContextOptions = array(
 							'ssl' => array(
@@ -26,8 +29,26 @@
 		$myVarValue = json_encode($result['GetReportSWStockResult']);
 	?>
 
-  var myvar = <?php echo $myVarValue; ?>;
-  var gg = myvar;
+    var myvar = <?php echo $myVarValue; ?>;
+    var data = myvar;
+
+    var obj = {};
+
+    obj.programName = 'PROGRAM NAME';
+    obj.template = 'sw';
+    obj.report = 'sw';
+    obj.program = 'sw';
+    obj.tableStyle = '';
+
+    $('#containTbData').createTable({
+        programName: obj.programName,
+        template: obj.template,
+        report: obj.report,
+        program: obj.program,
+        tableStyle: obj.tableStyle,
+        dataSource: data
+    });
+
 </script>
 
 </html>
