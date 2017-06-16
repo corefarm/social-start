4e8e7f11b268e8f4069afb2d1e68b3a09c9712f9
b6db07d1a5 <sitthichai.wa@gmail.com>
2017-06-16 15:55:58 +0700

    re

--- report/report.php
+++ report/report.php
@@ -36,6 +36,9 @@
 </body>
 <script src="../report/lib/jquery-2.1.0.min.js"></script>
 <script src="../report/lib/report.table.js"></script>
+<script src="../report/lib/moment.js"></script>
+<script src="../report/lib/bootstrap.min.js"></script>
+<script src="../report/lib/bootstrap-datetimepicker.min.js"></script>
 
 <script>
 
