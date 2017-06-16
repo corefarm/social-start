47cc4bca15f995cad1f7d3f00a29033828b082a6
b6db07d1a5 <sitthichai.wa@gmail.com>
2017-06-16 14:23:42 +0700

    re

--- report/report.php
+++ report/report.php
@@ -9,18 +9,6 @@
         </div>
     <div class="datagrid" id="datagrid">
         </div>
-		
-		<?php 
-		$id = $_GET['id'];
-		$cv = $_GET['cvFarm'];
-		$date = $_GET['date'];
-		
-		echo $id;
-		echo $cv;
-		echo $date;
-		
-		echo "Hello World!";
-		?>
 
 </body>
 <script src="../report/lib/jquery-2.1.0.min.js"></script>
@@ -30,12 +18,17 @@
 
 	<?php
 		
+		// $id = $_GET['id'];
+		// $cv = $_GET['cvFarm'];
+		// $date = $_GET['date'];
 		
-		
-		function retrieveReportData () {
+		function retrieveReportData ($id, $cv, $date) {
 			
-			$url = 'https://mservice-uat.cpf.co.th/Farm/FarmMobileRestService/FarmMobileRestService.svc/json/getreportswstock/123456789,2000020032-0-1-4-36,20170601';
+			//$url = 'https://mservice-uat.cpf.co.th/Farm/FarmMobileRestService/FarmMobileRestService.svc/json/getreportswstock/123456789,2000020032-0-1-4-36,20170601';
 
+			$url = 'https://mservice-uat.cpf.co.th/Farm/FarmMobileRestService/FarmMobileRestService.svc/json/getreportswstock/'.$id.','.$cv.','.$date;
+
+			
 			$arrContextOptions = array(
 								'ssl' => array(
 								'verify_peer' => false,
@@ -52,25 +45,25 @@
         
 	?>
 
-    //var myvar = <?php echo $myVarValue; ?>;
-    // var data = myvar;
+    var myvar = <?php echo retrieveReportData($_GET['id'], $_GET['cvFarm'], $_GET['date']); ?>;
+    var data = myvar;
 
-    // var obj = {};
+    var obj = {};
 
-    // obj.programName = 'รายงาน';
-    // obj.template = 'sw';
-    // obj.report = 'report';
-    // obj.program = 'sw';
-    // obj.tableStyle = '';
+    obj.programName = 'รายงาน';
+    obj.template = 'sw';
+    obj.report = 'report';
+    obj.program = 'sw';
+    obj.tableStyle = '';
 
-    // $('#datagrid').createTable({
-        // programName: obj.programName,
-        // template: obj.template,
-        // report: obj.report,
-        // program: obj.program,
-        // tableStyle: obj.tableStyle,
-        // dataSource: data
-    // });
+    $('#datagrid').createTable({
+        programName: obj.programName,
+        template: obj.template,
+        report: obj.report,
+        program: obj.program,
+        tableStyle: obj.tableStyle,
+        dataSource: data
+    });
 
 </script>
 
