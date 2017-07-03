5a2e04c8fc9ec569f6784c87b5aab9443e2f51af
b6db07d1a5 <sitthichai.wa@gmail.com>
2017-07-03 14:25:53 +0700

    cs

--- bot.php
+++ bot.php
@@ -1243,18 +1243,17 @@ function retrieveMsgDeadType($obj) {
 						]);
 					}
 					
-					error_log('CRS DETAIL >>>>>>>>>>>>>>>>>'.json_encode($crsDet));
+					//error_log('CRS DETAIL >>>>>>>>>>>>>>>>>'.json_encode($crsDet));
 					
 					array_push($crsDs,[
 						'title' => 'สาเหตุ',
 						'text' => '.',
 						'actions' => $crsdet
 					]);
+					
+					error_log('CRS DATASOURCE DETAIL >>>>>>>>>>>>>>>>>'.json_encode($crsDs));
 				}
 			}
-			
-			error_log('CRS DETAIL >>>>>>>>>>>>>>>>>'.json_encode($crsDs));
-			
 			
 			$ret = [
 				'msgType' => 'template',
